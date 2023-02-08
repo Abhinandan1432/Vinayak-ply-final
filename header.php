974cb90a58cb1bb2fe81f2daa2fcd9d48b9d942e
Abhinandan <Abhinandan1432>
2023-02-08 11:45:30 +0530

    update

--- header.php
+++ header.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>VINAYAK PLY</title>
+    <link rel="icon" type="image/x-icon" href="./assets/favicons/favicon.ico">
     <!-- bootstrap 5.0.2 css link -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">

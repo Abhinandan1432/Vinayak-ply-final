3e534cc1995ada0e67a3d389f565083e72ffc42a
Abhinandan <Abhinandan1432>
2023-02-08 11:13:41 +0530

    update

--- footer.php
+++ footer.php
@@ -98,12 +98,6 @@
 </footer>
 <!-- footer ends  -->
 
-
-
-
-
-
-
 <!-- bootstrap 5.0.2 js link -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"

c22e6c5fbbfab2e4ccdf96419e5b6fe3a34ba7e1
Abhinandan <Abhinandan1432>
2023-04-05 16:40:32 +0530

    certification page added

---
+++ certificate.php
@@ -0,0 +1,44 @@
+<?php require_once('header.php') ?>
+
+<!-- breadcrumb section starts -->
+<section class="about_breadcrumb">
+    <div class="container-fluid">
+        <div class="row">
+
+        </div>
+    </div>
+</section>
+<!-- breadcrumb section ends -->
+
+<!-- certifications sections -->
+<section class="certificates my-5">
+    <div class="container">
+        <h3 class="heading_three text-center mb-5 text-uppercase">We are Certified by</h3>
+        <div class="row">
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+            <div class="col-md-4 col-12 mb-4">
+                <img src="./assets/images/exhibition_one.png" class="img-fluid rounded" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- certifications sections -->
+
+
+<?php require_once('footer.php') ?>

96292be18a04064d1ee06dfbcfd2b0a2351a3396
DevByArun <arun48406@gmail.com>
2023-04-06 12:15:07 +0530

    content and div additional div remove

--- darpan.php
+++ darpan.php
@@ -2,79 +2,91 @@
 
 <!-- breadcrumb section starts -->
 <section class="about_breadcrumb">
-  <div class="container-fluid">
-    <div class="row">
+    <div class="container-fluid">
+        <div class="row">
 
+        </div>
     </div>
-  </div>
 </section>
 <!-- breadcrumb section ends -->
 
 
 <!-- darpan section start here -->
 <section class="darpan my-5">
-  <div class="container">
-    <h3 class="heading_three text-center mb-5">VinayakPly Darpan</h3>
-    <div class="row mb-5 shadow">
-      <div class="col-md-4 col-12 p-0">
-        <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
-      </div>
-      <div class="col-md-8 col-12">
-        <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
-        <p class="paragraph text-dark">
-          The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun activities
-          held in several places. Vellur Expo kicked off February with a successful market storming effort for new
-          channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of the trade
-          engagement. In addition, an interactive influencer meet was held in 12 different cities. Our on-the-ground
-          team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
+    <div class="container">
+        <h3 class="heading_three text-center mb-5">VinayakPly Darpan</h3>
+        <div class="row mb-5 shadow">
+            <div class="col-md-4 col-12 p-0">
+                <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
+            </div>
+            <div class="col-md-8 col-12">
+                <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
+                <p class="paragraph text-dark">
+                    The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun
+                    activities
+                    held in several places. Vellur Expo kicked off February with a successful market storming effort for
+                    new
+                    channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of
+                    the trade
+                    engagement. In addition, an interactive influencer meet was held in 12 different cities. Our
+                    on-the-ground
+                    team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
 
-        </p>
-      </div>
-    </div>
-    <div class="row mb-5 shadow">
-      <div class="col-md-4 col-12 p-0">
-        <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
-      </div>
-      <div class="col-md-8 col-12">
-        <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
-        <p class="paragraph text-dark">
-          The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun activities
-          held in several places. Vellur Expo kicked off February with a successful market storming effort for new
-          channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of the trade
-          engagement. In addition, an interactive influencer meet was held in 12 different cities. Our on-the-ground
-          team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
+                </p>
+            </div>
+        </div>
+        <div class="row mb-5 shadow">
+            <div class="col-md-4 col-12 p-0">
+                <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
+            </div>
+            <div class="col-md-8 col-12">
+                <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
+                <p class="paragraph text-dark">
+                    The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun
+                    activities
+                    held in several places. Vellur Expo kicked off February with a successful market storming effort for
+                    new
+                    channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of
+                    the trade
+                    engagement. In addition, an interactive influencer meet was held in 12 different cities. Our
+                    on-the-ground
+                    team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
 
-        </p>
-      </div>
-    </div>
-    <div class="row mb-5 shadow">
-      <div class="col-md-4 col-12 p-0">
-        <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
-      </div>
-      <div class="col-md-8 col-12">
-        <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
-        <p class="paragraph text-dark">
-          The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun activities
-          held in several places. Vellur Expo kicked off February with a successful market storming effort for new
-          channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of the trade
-          engagement. In addition, an interactive influencer meet was held in 12 different cities. Our on-the-ground
-          team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
+                </p>
+            </div>
+        </div>
+        <div class="row mb-5 shadow">
+            <div class="col-md-4 col-12 p-0">
+                <img src="./assets/images/darpan_img.png" class="img-fluid rounded w-100" alt="">
+            </div>
+            <div class="col-md-8 col-12">
+                <h5 class="heading_four">Vinayak Darpan Feb '22</h5>
+                <p class="paragraph text-dark">
+                    The February edition of the Vinayak Ply Darpan is now available. The month was jam-packed with fun
+                    activities
+                    held in several places. Vellur Expo kicked off February with a successful market storming effort for
+                    new
+                    channel partners. A branding and exposure push was done at over 45+ Dealers and Retailers as part of
+                    the trade
+                    engagement. In addition, an interactive influencer meet was held in 12 different cities. Our
+                    on-the-ground
+                    team increased by nine new employees. Click here to read: VinayakPly Darpan Feb '22
 
-        </p>
-      </div>
+                </p>
+            </div>
+        </div>
     </div>
-  </div>
 </section>
 <!-- darpan section start here -->
 
 <section class="paginations">
-  <div class="container">
-      <div class="row">
-          <nav aria-label="Page navigation example">
-              <ul id="pagination-demo" class="pagination justify-content-center"></ul>
-          </nav>
-      </div>
-  </div>
+    <div class="container">
+        <div class="row">
+            <nav aria-label="Page navigation example">
+                <ul id="pagination-demo" class="pagination justify-content-center"></ul>
+            </nav>
+        </div>
+    </div>
 </section>
 
 <?php require_once('footer.php') ?>
--- exhibition.php
+++ exhibition.php
@@ -57,7 +57,7 @@
             </div>
         </div>
     </div>
-    </div>
+
 </section>
 <!-- exibition section -->
 

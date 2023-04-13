1faa0d0821572a10cc74217b5ee0acf8f1355651
Abhinandan <Abhinandan1432>
2023-04-13 19:12:32 +0530

    move to top button updated

--- footer.php
+++ footer.php
@@ -124,6 +124,22 @@ lightGallery(document.getElementById('lightgallery'))
         return new bootstrap.Tooltip(tooltipTriggerEl)
     })
 </script>
+<script>
+    let mybutton = document.getElementById("moveTop");
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+</script>
 <!-- global js link -->
 <script src="./global.js"></script>
 <!-- global js link -->
--- global.js
+++ global.js
@@ -204,20 +204,7 @@ $(document).ready(function () {
 });
 
 //  move to top
-let mybutton = document.getElementById("moveTop");
-window.onscroll = function () { scrollFunction() };
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
 //  move to top
 
 // lightbox
